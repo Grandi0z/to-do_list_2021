376e38a3918271650a890b3e4bdcc4b277b6dcf0
unknown <josephlkausi@gmail.com>
2023-04-07 18:08:22 +0200

    Add Edit and save task option

--- index.js
+++ index.js
@@ -6,11 +6,49 @@ window.addEventListener('load',() => {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         const newTask = inputNewTask.value;
-        console.log(inputNewTask)
         if(!newTask) {
-            alert("please fill the task name")
+            alert("please fill out the task")
+            return
         }else {
-            console.log("success")
+            const tasks = document.createElement("div")
+            tasks.setAttribute("id","tasks")
+ 
+            const task = document.createElement("div")
+            task.classList.add("task")
+            tasks.appendChild(task)
+ 
+            const content = document.createElement("div")
+            content.innerHTML=`
+                         <div class="content">
+                             <input type="text" class="taskDescription" value="${newTask}" read-only>  
+                         </div>`
+             task.appendChild(content)
+             tasksList.appendChild(tasks)
+             
+             const action = document.createElement("div")
+             action.classList.add("action")
+             action.innerHTML=`
+                        <button id="edit">Edit</button>
+                        <button id="delete">Delete</button>
+             `
+             task.appendChild(action)
+
+            // EDIT
+            const editBtn = document.getElementById("edit")
+            editBtn.addEventListener('click',()=>{
+                const description = document.querySelector(".taskDescription")
+                if(editBtn.innerText.toLowerCase()==="edit"){
+                    description.removeAttribute('read-only')
+                    description.focus()
+                    editBtn.innerText="SAVE" 
+                }else{
+                    description.setAttribute('read-only','read-only')
+                    editBtn.innerText="EDIT" 
+                }
+                
+            })
+
         }
+
     } )
 })
